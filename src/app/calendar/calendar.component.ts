9406dfa383058c32fedc215193045b7f58fb719f
Jesse Tan <tanleyang7@gmail.com>
2023-04-22 16:40:51 -0500

    this doesn't work

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NgbDateStruct, NgbCalendar, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+	selector: 'ngbd-datepicker-basic',
+	standalone: true,
+	imports: [NgbDatepickerModule, FormsModule, JsonPipe],
+	templateUrl: './datepicker-basic.html',
 })
-export class CalendarComponent {
+export class NgbdDatepickerBasic {
+	model: NgbDateStruct;
+	date: { year: number; month: number };
 
+	constructor(private calendar: NgbCalendar) {}
+
+	selectToday() {
+		this.model = this.calendar.getToday();
+	}
 }
