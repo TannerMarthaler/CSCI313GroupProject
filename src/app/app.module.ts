0c7e86508fe17e87f564353b1c07b0bab24cea24
LanceBalstad <Balstad4l@gmail.com>
2023-04-22 18:51:20 -0500

    sign in and register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
