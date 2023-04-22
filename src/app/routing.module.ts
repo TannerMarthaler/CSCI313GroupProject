f6779a822b4ba08f2ac0f570f92de8b71f1db440
Tanner Marthaler <tanner.marthaler@ndus.edu>
2023-04-22 16:30:19 -0500

    Fixing stuff

---
+++ src/app/routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const ROUTES : Routes = [
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class RoutingModule { }
