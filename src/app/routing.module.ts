4f9597a57c8d3ac62521c8831da3b89e0f35759a
Tanner Marthaler <tanner.marthaler@ndus.edu>
2023-04-28 14:44:38 -0500

    Fixing visual bugs

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -6,7 +6,13 @@ import { AppComponent } from './app.component';
 
 
 const ROUTES : Routes = [
-  { path : '', component : AppComponent }
+  { path : '', component : AppComponent },
+  // { path : '/menu', component : MenuComponent },
+  // { path : '/locations', component : LocationComponent },
+  // { path : '/calendar', component : CalendarComponent },
+  // { path : '/about-us', component : AboutUsComponent },
+  // { path : '/contact-us', component : ContactUsComponent },
+  // { path : '/sign-in', component : SignInComponent },
 ];
 
 @NgModule({
