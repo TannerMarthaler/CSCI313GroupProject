ea2ee96d7916c1711da0865a411d06eeeba961e6
Jesse175 <tanleyang7@gmail.com>
2023-05-03 18:48:39 -0500

    added events to calendar hopefully this wont break

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -18,23 +18,39 @@ export class CalendarComponent {
   setView(view: CalendarView) {
     this.view = view;
   }
-
-
   
   events: CalendarEvent[] = [
     {
-      start: (new Date()),
-      title: 'First event',
+      start: (new Date("2023-05-01T16:00:00")),
+      end: (new Date("2023-05-01T21:30:00")),
+      title: "Women's Night",
     },
     {
-      start: startOfDay(new Date()),
-      title: 'Second event',
+      start: (new Date("2023-05-02T16:00:00")),
+      end: (new Date("2023-05-02T21:30:00")),
+      title: "Men's Night",
     },
     {
-      start: (new Date("2023-04-13T15:30:00")),
-      end: (new Date("2023-04-13T17:30:00")),
-      title: 'test',
-    }
+      start: (new Date("2023-05-13T15:30:00")),
+      end: (new Date("2023-05-13T18:30:00")),
+      title: 'Music Night',
+    },
+    {
+      start: (new Date("2023-05-11T10:00:00")),
+      end: (new Date("2023-05-11T14:30:00")),
+      title: 'Milkshake Frenzy',
+    },
+    {
+      start: (new Date("2023-05-13T17:30:00")),
+      end: (new Date("2023-05-13T22:00:00")),
+      title: 'Smash Ultimate Singles',
+    },
+    {
+      start: (new Date("2023-05-30T15:30:00")),
+      end: (new Date("2023-05-30T17:30:00")),
+      title: 'Karaoke Night for the Mute',
+    },
+
   ]
 
 
