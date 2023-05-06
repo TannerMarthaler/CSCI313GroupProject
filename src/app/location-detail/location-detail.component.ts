5bca9dba8058f55939c6abf4fb6a8fef6e644385
LanceBalstad <Balstad4l@gmail.com>
2023-05-05 19:55:04 -0500

    location moorhead

--- src/app/location-detail/location-detail.component.ts
+++ src/app/location-detail/location-detail.component.ts
@@ -32,51 +32,9 @@ export class LocationDetailComponent {
 
   ngOnInit() {
     this.fargoList = this.locationService.getFargoItems();
+    this.moorheadList = this.locationService.getMoorheadItems();
     this.selectedLocation = this.locationService.getLocationSearch();
   }
-
-
-  // pcFargoList: pcFargo[] = [
-  //   {
-  //     id: 0, location: 'Fargo', ram: '16 GB', gpu: 'AMD Radeon RX 6600', processor: 'AMD Ryzen 5 5500'
-  //   },
-  //   {
-  //     id: 1, location: 'Fargo', ram: '8 GB', gpu: 'GeForce 1660 Super', processor: 'Intel Core i9'
-  //   },
-  //   {
-  //     id: 2, location: 'Fargo', ram: '12 GB', gpu: 'Nvidia Titan RTX', processor: 'AMD Ryzen 7 5700'
-  //   },
-  //   {
-  //     id: 3, location: 'Fargo', ram: '16 GB', gpu: 'GeForce RTX 4070', processor: 'Intel Core i3'
-  //   },
-  //   {
-  //     id: 4, location: 'Fargo', ram: '12 GB', gpu: 'GeForce 1660 Super', processor: 'AMD Ryzen 5 5500'
-  //   },
-  //   {
-  //     id: 5, location: 'Fargo', ram: '12 GB', gpu: 'Nvidia Titan RTX', processor: 'AMD Ryzen 7 5700'
-  //   },
-  //   {
-  //     id: 6, location: 'Fargo', ram: '16 GB', gpu: 'GeForce RTX 4070', processor: 'Intel Core i3'
-  //   },
-  //   {
-  //     id: 7, location: 'Fargo', ram: '12 GB', gpu: 'GeForce 1660 Super', processor: 'AMD Ryzen 5 5500'
-  //   }
-  // ];
-
-  // pcMoorheadList: pcMoorhead[] = [
-  //   {
-  //     id: 0, location: 'Moorhead', ram: '12 GB', gpu: 'AMD Radeon RX 6600', processor: 'AMD Ryzen 5 5500'
-  //   },
-  //   {
-  //     id: 1, location: 'Moorhead', ram: '16 GB', gpu: 'GeForce 1660 Super', processor: 'Intel Core i9'
-  //   },
-  //   {
-  //     id: 2, location: 'Moorhead', ram: '8 GB', gpu: 'Nvidia Titan RTX', processor: 'AMD Ryzen 7 5700'
-  //   },
-  //   {
-  //     id: 3, location: 'Moorhead', ram: '16 GB', gpu: 'GeForce RTX 4070', processor: 'Intel Core i3'
-  //   }
-  // ];
 
   fargoCount: number = 8;
   moorheadCount: number = 4;
